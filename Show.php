1d0bfd2c5c8c62135be7766a1ed7376a939ac1e4
IntanonTangtung <73689440+IntanonTangtung@users.noreply.github.com>
2020-11-19 20:43:46 +0700

    Add files via upload

--- Show.php
+++ Show.php
@@ -1,15 +1,20 @@
 <html>
 <head>
-    <title>ITF Lab</title>
-    <meta charset="utf-8">
+<title>ITF Lab Show information</title>
+<meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<style>
+body {
+  background-color: rgba(172, 207, 246, 1);
+}
+</style>
 </head>
 <body>
-    <?php
+<?php
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'lab63070191.mysql.database.azure.com', 'Intanon@lab63070191', 'Efc23b2f', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
@@ -18,32 +23,28 @@ if (mysqli_connect_errno($conn))
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-    <table width="600" border="1" class="table table-dark table-striped">
-        <tr>
-            <th width="100">
-                <div align="center">Name</div>
-            </th>
-            <th width="350">
-                <div align="center">Comment </div>
-            </th>
-            <th width="150">
-                <div align="center">Link </div>
-            </th>
-        </tr>
-        <?php
+<center>
+<table class="table table-hover table-bordered table-center" style="width:800px">
+  <tr class="table-primary" >
+    <th>Name</th>
+    <th>Comment</th>
+    <th>Link</th>
+  </tr>
+<center\>
+<?php
 while($Result = mysqli_fetch_array($res))
 {
 ?>
-        <tr>
-            <td><?php echo $Result['Name'];?></div></td>
-            <td><?php echo $Result['Comment'];?></td>
-            <td><?php echo $Result['Link'];?></td>
-        </tr>
-        <?php
+  <tr class="table-primary">
+    <td><?php echo $Result['Name'];?></div></td>
+    <td><?php echo $Result['Comment'];?></td>
+    <td><?php echo $Result['Link'];?></td>
+  </tr>
+<?php
 }
 ?>
-    </table>
-    <?php
+</table>
+<?php
 mysqli_close($conn);
 ?>
 <a href="https://intanon.azurewebsites.net/form.html" class="btn btn-info">add</a>
