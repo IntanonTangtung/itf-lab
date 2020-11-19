0a15eba5d650eaa8e8b605f519161cbe22d38de1
IntanonTangtung <73689440+IntanonTangtung@users.noreply.github.com>
2020-11-19 20:25:31 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -33,7 +33,7 @@
 
     if (mysqli_query($conn, $sql)) {
       ?><div class="display-3 text-center mb-3">Thank For Filling Your Information</div>
-      <div class="text-center"><a href="index.php" class="btn btn-warning"><i class="fa fa-home"></i></a></div><?php
+      <div class="text-center"><a href="Show.php" class="btn btn-warning"><i class="fa fa-home"></i></a></div><?php
       } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
       }
