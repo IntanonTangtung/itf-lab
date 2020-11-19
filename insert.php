18489457ffddb096f097da503d2421a99b6afebf
IntanonTangtung <73689440+IntanonTangtung@users.noreply.github.com>
2020-11-19 19:01:31 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -31,8 +31,32 @@
     $sql = "INSERT INTO guestbook (Name, Comment) VALUES ('$name', '$comment')";
 
 
-    if (mysqli_query($conn, $sql)) 
-      <div class="display-3 text-center mb-3">Thanks For Filling Your Information</div>
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'lab63070191.mysql.database.azure.com', 'Intanon@lab63070191', 'Efc23b2f', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$name = $_POST['name'];
+$comment = $_POST['comment'];
+$link = $_POST['link'];
+
+
+$sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment', '$link')";
+
+
+if (mysqli_query($conn, $sql)) {
+    echo "Thanks For Add Your Information";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>
     
   </body>
 </html>
